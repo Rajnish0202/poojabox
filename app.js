c3c7a144561670bbe2361eb6b0acc5a4a75ed1dd
Rajnish0202 <rajnish.0202kumar@gmail.com>
2022-05-12 17:00:23 +0530

    completed

--- app.js
+++ app.js
@@ -4,14 +4,6 @@ const homeDown = document.querySelector(".homeDown");
 const shopDown = document.querySelector(".shopDown");
 const Section = document.querySelector("section");
 
-// const paragraph = document.querySelectorAll(".products .content h6");
-// // Slicing the Paragrap
-
-// paragraph.forEach((para) => {
-//   para.innerHTML.slice(0, 19) + `...`;
-//   // console.log(para.innerHTML.slice(0, 19) + `...`);
-// });
-
 home.addEventListener("mouseover", () => {
   homeDown.classList.add("active");
   shopDown.classList.remove("active");
@@ -51,4 +43,81 @@ var swiper = new Swiper(".mySwiper", {
       slidesPerView: 6,
     },
   },
+});
+
+// Side bar
+
+const sidebar = document.querySelector(".sidebar");
+const Bar = document.querySelector(".bar");
+const Close = document.querySelector(".close");
+const Search = document.querySelector(".search");
+const SearchDiv = document.querySelector(".search-div");
+// console.log(sidebar, Bar, Search, SearchDiv);
+
+Bar.addEventListener("click", () => {
+  sidebar.classList.add("show");
+  sidebar.classList.remove("hide");
+});
+
+Close.addEventListener("click", () => {
+  sidebar.classList.remove("show");
+  sidebar.classList.add("hide");
+});
+
+Search.addEventListener("click", () => {
+  SearchDiv.classList.toggle("active");
+});
+
+// Side Product toggle
+
+const SideProduct = document.querySelectorAll(".side-content");
+// console.log(SideProduct);
+
+SideProduct.forEach((product) => {
+  const Products = product.querySelector(".side-products-box");
+  const Icon = product.querySelector("i");
+  // console.log(Icon);
+  Products.classList.remove("active");
+  Icon.classList.remove("fa-minus");
+  product.addEventListener("click", () => {
+    Products.classList.toggle("active");
+    Icon.classList.toggle("fa-plus");
+    Icon.classList.toggle("fa-minus");
+  });
+  // console.log(Products);
+});
+
+// On Scroll Header Fixed
+
+const Header = document.querySelector("header");
+const Top = document.querySelector(".top");
+const Logo = document.querySelector("#logo");
+const LogoScroll = document.querySelector("#logoScroll");
+
+window.addEventListener("scroll", () => {
+  if (scrollY >= 100) {
+    Header.classList.add("active");
+    Top.classList.add("active");
+    Logo.classList.add("active");
+    LogoScroll.classList.add("active");
+    homeDown.classList.remove("active");
+    shopDown.classList.remove("active");
+  } else {
+    Header.classList.remove("active");
+    Top.classList.remove("active");
+    Logo.classList.remove("active");
+    LogoScroll.classList.remove("active");
+  }
+});
+
+// Floating Top
+
+const Float = document.querySelector(".floatingTop");
+
+window.addEventListener("scroll", () => {
+  if (scrollY >= 400) {
+    Float.classList.add("active");
+  } else {
+    Float.classList.remove("active");
+  }
 });
